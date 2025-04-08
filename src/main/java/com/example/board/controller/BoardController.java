2a50411921626589c9f2cfc35e7208a231f8737d
jade2 <jade2180@naver.com>
2025-04-09 00:23:36 +0900

    second commit

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.example.board.controller;
 
 import com.example.board.dto.BoardDTO;
+import com.example.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/board")  // board로 시작하는 주소를 컨트롤러가 받음
 public class BoardController {
+    private final BoardService boardService; // 생성자 주입방식으로 의존성 주입받음
+
     @GetMapping("/save")   // 매핑 값이 일치하는 메서드가 호출
     public String saveForm(){
         return "save";
@@ -19,6 +22,8 @@ public class BoardController {
                         // save.html , DTO의 필드값 이름이 같을 때
                         // 간단하게 입력값을 가져올 수 있음
         System.out.println("boardDTO = " + boardDTO);
+        boardService.save(boardDTO);  // boardService의 save라는 메서드 호출
+
         return null;
     }
 
--- src/main/java/com/example/board/service/BoardService.java
+++ src/main/java/com/example/board/service/BoardService.java
@@ -1,4 +1,16 @@
 package com.example.board.service;
 
+import com.example.board.dto.BoardDTO;
+import com.example.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class BoardService {
+    private final BoardRepository boardRepository;
+
+
+    public void save(BoardDTO boardDTO) {  // save 메서드를 service에 정의
+    }
 }
